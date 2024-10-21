d9960a80cb29b0828a7d7e9cc99e31e2b2dd3a6e
vialvarez0603 <vialvarez@itba.edu.ar>
2024-10-21 10:25:25 -0300

    Rename 2.10.sql to 10_Infomed_TP5_AlvarezChahwan.sql

---
+++ 10_Infomed_TP5_AlvarezChahwan.sql
@@ -0,0 +1,4 @@
+SELECT *
+FROM consultas
+WHERE id_medico = 3
+AND fecha >= '2024-08-01' AND fecha < '2024-09-01';
--- 2.10.sql
+++
@@ -1,4 +0,0 @@
-SELECT *
-FROM consultas
-WHERE id_medico = 3
-AND fecha >= '2024-08-01' AND fecha < '2024-09-01';
